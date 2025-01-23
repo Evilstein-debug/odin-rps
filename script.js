fc0696ed547d4912813ef1bc99a8f3577c43ffc1
Evilstein-debug <tejaspathak2005@gmail.com>
2025-01-24 03:36:20 +0530

    rps2

--- script.js
+++ script.js
@@ -1,26 +1,5 @@
-// function getComputerChoice(){
-//     let choice = Math.random();
-//     if (choice >= 0 && choice < 1/3) return "rock";
-//     else if (choice >= 1/3 && choice < 2/3) return "paper";
-//     else return "scissors";
-// }
-
-
-// function getHumanChoice(){
-//    let humanChoice = prompt("Enter your choice >.< ").toLowerCase();
-//     if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors") return humanChoice;
-//     else alert("enter a valid choice between rock, paper or scissors");
-// }
-
-
-// let humanScore = 0;
-// let computerScore = 0;
-
-
-
-// const humanSelection = getHumanChoice();
-// const computerSelection = getComputerChoice();
-
+let humanScore = 0;
+let computerScore = 0;
 
 function playGame(){
 
@@ -38,17 +17,8 @@ function playGame(){
         else alert("enter a valid choice between rock, paper or scissors");
     }
 
-
-
-    let humanScore = 0;
-    let computerScore = 0;
-
-
-
     const humanSelection = getHumanChoice();
     const computerSelection = getComputerChoice();
-
-
 
     function playRound(humanChoice, computerChoice){
         if (humanChoice === "rock" && computerChoice === "scissors") {
@@ -76,11 +46,18 @@ function playGame(){
             return "You win! Rock beats scissors.";
         }
         else return "draw!";
+        
     }
-    for(let i=0; i<5; i++){
-        console.log(playRound(humanSelection, computerSelection));
-    }
-    if(humanScore>computerScore) return "The final victory is yours!";
-    else if (humanScore<computerScore) return "Better luck next time!";
+    console.log(playRound(humanSelection, computerSelection));
 }
+
+
+for(let i=0; i<5; i++){
 console.log(playGame());
+}
+
+console.log("Score: You - Opp");
+console.log(humanScore, " - ",computerScore);
+if (humanScore>computerScore) console.log("The final victory is yours!");
+else if (humanScore<computerScore) console.log("Better luck next time!");
+else console.log("finally its a draw!");
